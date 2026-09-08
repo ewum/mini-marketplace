dcc860199864675efc411033c9449af7ed2865ed
ewum <alanwvv@gmail.com>
2026-09-08 02:29:44 -0300

    refactor: add routes files

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
 app.use(express.static('public'));
+
+app.use('/api/products', require('./routes/products'));
 
 app.get('*', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
